d9a970864b048c4d56d072fdd842921ddc16ae79
moezbj <moezbj.93@gmail.com>
2018-02-15 16:24:06 +0100

    fix checkBox

--- src/components/CheckBox.js
+++ src/components/CheckBox.js
@@ -1,60 +1,46 @@
 import React, { Component } from 'react';
-import { Text, TouchableHighlight, View } from 'react-native';
+import { Text, TouchableHighlight, View, StyleSheet } from 'react-native';
 
 import Icon from 'react-native-vector-icons/FontAwesome';
 
-class Checkbox extends Component {
+export default class Checkbox extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
-      backgroundColor: '#FFF',
-      borderRadius: 0,
-      borderWidth: 2,
       checked: false,
-      color: '#000',
-      margin: 2,
       name: '',
-      onChange: null,
-
-      borderColor: '#000'
+      onChange: null
     };
   }
-
   render() {
-    BORDER_RADIUS = this.state.borderRadius;
-    BORDER_WIDTH = this.state.borderWidth;
-    COLOR = this.state.color;
-    MARGIN = this.state.margin;
-    BORDER_COLOR = this.state.borderColor;
     return (
       <TouchableHighlight
         underlayColor="transparent"
         onPress={() => {
-          this._toggleCheck();
+          this.toggleCheck();
         }}
-        style={{
-          backgroundColor: '#fff',
-          borderColor: BORDER_COLOR,
-          borderRadius: BORDER_RADIUS,
-          borderWidth: BORDER_WIDTH,
-          height: 20,
-          margin: MARGIN,
-          width: 20
-        }}
+        style={styles.check}
       >
         <View style={{ flex: 1, alignItems: 'center' }}>
-          {this.state.checked && <Icon name="check" size={20 - 5} color={COLOR} />}
+          {this.state.checked && <Icon name="check" size={20 - 5} color="blue" />}
         </View>
       </TouchableHighlight>
     );
   }
-
-  _toggleCheck() {
+  toggleCheck() {
     const checked = !this.state.checked;
     this.setState({ checked });
     this.props.onChange && this.props.onChange(this.props.name, checked);
   }
 }
-
-module.exports = Checkbox;
+const styles = StyleSheet.create({
+  check: {
+    backgroundColor: '#fff',
+    borderColor: 'blue',
+    borderRadius: 6,
+    borderWidth: 2,
+    height: 20,
+    margin: 2,
+    width: 20
+  }
+});
